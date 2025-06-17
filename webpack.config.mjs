998a75a411ae47d1caadf743d30ceb7652fa536f
Darya Sevostyanova <sevostyanova2016@icloud.com>
2025-06-17 15:01:38 +0300

    editing dist

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -8,7 +8,7 @@ export default {
   mode: 'development',
   entry: './src/script.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
     clean: true,
   },
